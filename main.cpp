daf6db1ef9f98a9ce53e6b821b344b1c1b18548a
antoine <antoine.rato@gmail.com>
2017-11-22 20:55:36 +0100

    Travis test for Evan V2

--- main.cpp
+++ main.cpp
@@ -1,22 +1,53 @@
-#include <algorithm>
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <list>
 #include <map>
+#include <algorithm>
 
-/* Antoine Rato */
 
 using namespace std;
 
-// source: https://stackoverflow.com/questions/6089231/getting-std-ifstream-to-handle-lf-cr-and-crlf
+// Fonction toute faite trouvé sur stackoverflow
+
+
+/*void create_vector ( std::ifstream & file, std::vector < std::string > & v ) {
+	if ( file ) {
+		std::string line;
+		while ( std::getline ( file, line ) ) {
+			v.push_back ( line );
+		}
+	}
+	else {
+		std::cerr << "Erreur d'ouverture du fichier" << std::endl;
+	}
+}
+
+const std::string value_user ( std::vector < std::string > & valUser ) {
+	std::string str;
+	std::string valTemp;
+	for(auto v : valUser) {
+        valTemp += v;
+	}
+	std::cout << "Entrer une chaine de caractere : " << valTemp;
+	std::cin >> str;
+	valUser.push_back(str);
+	return valTemp+str;
+}
+
+void search_value ( const std::string & value, const std::vector < std::string > & v ) {
+	for ( std::string t : v ) {
+		if ( t.find ( value ) == std::string::npos ) {
+
+		}
+		else {
+			std::cout << t << std::endl;
+		}
+	}
+}*/
+
+
 std::istream &safeGetline(std::istream &is, std::string &t) {
     t.clear();
-
-    // The characters in the stream are read one-by-one using a std::streambuf.
-    // That is faster than reading them one-by-one using the std::istream.
-    // Code that uses streambuf this way must be guarded by a sentry object.
-    // The sentry object performs various tasks,
-    // such as thread synchronization and updating the stream state.
 
     std::istream::sentry se(is, true);
     std::streambuf *sb = is.rdbuf();
@@ -31,7 +62,7 @@ std::istream &safeGetline(std::istream &is, std::string &t) {
                     sb->sbumpc();
                 return is;
             case EOF:
-                // Also handle the case when the last line has no line ending
+
                 if (t.empty())
                     is.setstate(std::ios::eofbit);
                 return is;
@@ -41,54 +72,45 @@ std::istream &safeGetline(std::istream &is, std::string &t) {
     }
 }
 
-
-int main(int argc, char *argv[])
+int main (int argc, char * argv[])
 {
-    if(argc != 3)
-    {
-        cout << "Add a dictionary on input." << endl;
-        return -1;
-    }
+    std::string word;
+    std::string wordConvert;
+    std::string valUSer;
+    std::multimap<string, string>::iterator it2;
 
-    multimap<string, string> multimapOfDictionary;
-    string nameOfDictionary = argv[2];
-    string input;
+    // if ( argc > 0 ) {
+    //     std::cout << argv [ 0 ] << std::endl;
+    // }
 
-    ifstream dictionary(nameOfDictionary.c_str(), ios::in);
+    std::ifstream file { argv [ 2 ]};
 
-    //Initialisation of the
-    if (dictionary.fail())
-    {
-        cout << "This dictionary doesn't exist." << endl;
-        return -1;
-    }
-    else
-    {
-        string line, lineConvert;
-        while(!safeGetline(dictionary, line).eof())
-        {
-            lineConvert = line;
-            transform(lineConvert.begin(), lineConvert.end(), lineConvert.begin(), ::tolower);
-            sort(lineConvert.begin(), lineConvert.end());
-            multimapOfDictionary.insert(pair<string,string>(lineConvert, line));
+    multimap<string, string> dictionnary;
+    //FORMAT DOS ET UNIX
+    if(file) {
+
+        while (!safeGetline(file, word).eof()){
+            wordConvert = word;
+            transform(wordConvert.begin(), wordConvert.end(), wordConvert.begin(), ::tolower);
+            sort(wordConvert.begin(), wordConvert.end());
+            dictionnary.insert(make_pair(wordConvert, word));
         }
     }
+    while(true) {
 
-    while(true)
-    {
-        cin >> input;
-        
-        //Conver & Sort
-        transform(input.begin(), input.end(), input.begin(), ::tolower);
-        sort(input.begin(), input.end());
+        cin >> valUSer;
 
-        pair <multimap<string, string>::iterator, multimap<string, string>::iterator> pairIterator = multimapOfDictionary.equal_range(input);
+        transform(valUSer.begin(), valUSer.end(), valUSer.begin(), ::tolower);
 
-        for (multimap<string, string>::iterator i = pairIterator.first; i != pairIterator.second; ++i)
-        {
-            cout << i->second << endl;
+        sort(valUSer.begin(), valUSer.end());
+
+        pair<multimap<string,string>::iterator, multimap<string,string>::iterator> it1;
+
+        it1 = dictionnary.equal_range(valUSer);
+
+        for(it2 = it1.first; it2 != it1.second; ++it2){
+            cout<<it2->second<<endl;
         }
     }
-
     return 0;
 }
